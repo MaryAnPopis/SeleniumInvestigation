f3801755b1ce946a63215dbe4216b2caed97843c
Leonardo Mora <jleomoram@gmail.com>
2019-11-22 00:50:43 -0600

    added tc-2 and refactored some tc-1 methods for common use, as well as commenting some parts of tc-1's methods

--- src/test/java/TestNopCommerce.java
+++ src/test/java/TestNopCommerce.java
@@ -25,6 +25,9 @@ import java.util.Map;
 public class TestNopCommerce {
     private WebDriver chrome;
     private WebDriver firefox;
+    private WebDriverWait loadWait;
+    private WebDriverWait alertWait;
+    private ExpectedCondition<Boolean> domIsReady;
     private static final String WEB_PAGE_URL = "https://demo.nopcommerce.com";
 
     @BeforeClass
@@ -41,6 +44,17 @@ public class TestNopCommerce {
         // Open page in different browsers
         chrome.get(WEB_PAGE_URL);
         firefox.get(WEB_PAGE_URL);
+    }
+
+    @Before
+    public void setupWaits() {
+        loadWait = new WebDriverWait(firefox, 10);
+        alertWait = new WebDriverWait(firefox, 1);
+        domIsReady = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor) driver).executeScript("return document.readyState").toString().equals("complete");
+            }
+        };
     }
 
     @After
@@ -156,16 +170,9 @@ public class TestNopCommerce {
 
     //    Second investigation tests start here.
 
+    //TC-1
     @Test
     public void testCategories() {
-
-        final WebDriverWait wait = new WebDriverWait(chrome, 20);
-        final WebDriverWait alertWait = new WebDriverWait(chrome, 1);
-        final ExpectedCondition<Boolean> domIsReady = new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver driver) {
-                return ((JavascriptExecutor) driver).executeScript("return document.readyState").toString().equals("complete");
-            }
-        };
 
         //Hashtable with the categories
         Hashtable<String, String> nameOfCategories = new Hashtable<String, String>();
@@ -180,25 +187,86 @@ public class TestNopCommerce {
         for (Map.Entry<String, String> category : nameOfCategories.entrySet()) {
             System.out.println(category);
             // Click on Menu tab Chrome
-            chrome.findElement(By.linkText(category.getKey())).click();
+            firefox.findElement(By.linkText(category.getKey())).click();
             // Wait for the page to load
-            wait.until(domIsReady);
+            loadWait.until(domIsReady);
             // Check the current url after click
-            assertEquals(chrome.getCurrentUrl(), category.getValue());
+            assertEquals(firefox.getCurrentUrl(), category.getValue());
         }
 
-        ((JavascriptExecutor) chrome).executeScript("alert('The test case has been executed succesfully')");
+        // Raise an alert with the requested message: "The test case has been executed successfully
+        ((JavascriptExecutor) firefox).executeScript("alert('The test case has been executed successfully')");
 
-        while (isAlertPresent(alertWait));
+        // Wait until the alert is dismissed
+        while (isAlertPresent());
     }
 
-    private boolean isAlertPresent(WebDriverWait wait) {
+    private boolean isAlertPresent() {
         try {
-            wait.until(ExpectedConditions.alertIsPresent());
+            alertWait.until(ExpectedConditions.alertIsPresent());
             return true;
         } catch (TimeoutException e) {
             return false;
         }
+    }
+
+    @Test
+    public void testShoppingCart() {
+        String wishListEmptyMessage = "The wishlist is empty!";
+        String productTitle = "Fahrenheit 451 by Ray Bradbury";
+        String skuFahrenheit = "FR_451_RB";
+        String bookToSearch = "Fahrenheit 45";
+
+        By wishlistSelector = By.cssSelector(".ico-wishlist");
+        By noDataSelector = By.cssSelector(".no-data");
+        By searchBarSelector = By.xpath("//input[@id='small-searchterms']");
+        By searchButtonSelector = By.xpath("//input[@class='button-1 search-box-button']");
+        By wishListButtonSelector = By.cssSelector("#add-to-wishlist-button-37");
+        By skuBookSelector = By.className("sku-number");
+        By whishLitTopBannerSelector = By.cssSelector("a[href='/wishlist']");
+        By addToCartCheckBoxName = By.name("addtocart");
+        By continueShoppingButtonName = By.name("continueshopping");
+        By addToCartSelector = By.className("wishlist-add-to-cart-button");
+
+        firefox.findElement(wishlistSelector).click();
+
+        // Check if the list is empty
+        assertEquals(firefox.findElement(noDataSelector).getText(), wishListEmptyMessage);
+
+        // Search book in the search bar
+        firefox.findElement(searchBarSelector).sendKeys(bookToSearch);
+        firefox.findElement(searchButtonSelector).click();
+
+        // Click in the book and go to the page details
+        firefox.findElement(By.linkText(productTitle)).click();
+
+        // Add book in to the wishlist
+        firefox.findElement(wishListButtonSelector).click();
+
+        // Click back to the wishlist in the green top banner
+        firefox.findElement(whishLitTopBannerSelector).click();
+
+        // Check if the Fahrenheit 451 is in the wish list
+        assertEquals(firefox.findElement(skuBookSelector).getText(), skuFahrenheit);
+
+        // Select the book to be added to the cart
+        firefox.findElement(addToCartCheckBoxName).click();
+
+        // Add the selected book to the cart
+        firefox.findElement(addToCartSelector).click();
+
+        // Assert if the book added to the cart is the same as the searched book
+        assertEquals(firefox.findElement(skuBookSelector).getText(), skuFahrenheit);
+
+        // Navigate to continue shopping
+        firefox.findElement(continueShoppingButtonName).click();
+
+        // Raise an alert with the requested message: "The test case # 2 has been executed successfully"
+        ((JavascriptExecutor) firefox).executeScript("alert('The test case # 2 has been executed successfully')");
+
+        // Wait until the alert is dismissed
+        while (isAlertPresent()) ;
+
     }
 
 }
